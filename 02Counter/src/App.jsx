2651f4f7ab89d0408ce5d3a46a9302e255482ef8
vishal lokhande <lvishal108@gmail.com>
2025-03-23 20:00:16 +0000

    Create new Project Counter js

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './Count.css'; // Import the CSS file
+
+function Count() {
+  const [count, setCount] = useState(10);
+
+  const AddValue = () => {
+    setCount(count + 1);
+  }
+
+  const SubValue = () => {
+    setCount(count - 1);
+  }
+
+  return (
+    <>
+      <h1>Count Value: {count}</h1>
+      <button className="button increase" onClick={AddValue}>Increase Value</button>
+      <button className="button decrease" onClick={SubValue}>Decrease Value</button>
+    </>
+  );
+}
+
+export default Count;
